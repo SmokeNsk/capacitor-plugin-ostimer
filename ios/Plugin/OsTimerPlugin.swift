9e4f9b9370b6ec3a7156f2dcbaba44d2fd17df9d
SmokeNsk <109534426+SmokeNsk@users.noreply.github.com>
2022-08-22 17:05:12 +0700

    ios
    start add ios code

--- ios/Plugin/OsTimerPlugin.swift
+++ ios/Plugin/OsTimerPlugin.swift
@@ -8,11 +8,21 @@ import Capacitor
 @objc(OsTimerPlugin)
 public class OsTimerPlugin: CAPPlugin {
     private let implementation = OsTimer()
-
+/*
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
         call.resolve([
             "value": implementation.echo(value)
         ])
+    }
+*/
+    @objc func start(_ call: CAPPluginCall){
+        let delay=Int(call.getString("delay")?? getConfigValue("delay") as? string)?? 1500
+        let period=Int(call.getString("delay")?? getConfigValue("delay") as? string) ?? 1500
+        call.resolve()
+    }
+
+    @objc func stop(_ call: CAPPluginCall){
+        call.resolve()
     }
 }
