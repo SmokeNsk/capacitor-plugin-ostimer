21c1aad6ac36275507fe6491e6252f7f82b21223
SmokeNsk <109534426+SmokeNsk@users.noreply.github.com>
2022-08-23 15:39:00 +0700

    Update OsTimer.swift

--- ios/Plugin/OsTimer.swift
+++ ios/Plugin/OsTimer.swift
@@ -10,7 +10,7 @@ import Foundation
 /// RepeatingTimer mimics the API of DispatchSourceTimer but in a way that prevents
 /// crashes that occur from calling resume multiple times on a timer that is
 /// already resumed (noted by https://github.com/SiftScience/sift-ios/issues/52
-@objc public class OsTimer {
+@objc public class OsTimer : NSObject {
 
     var timeInterval: TimeInterval
     
