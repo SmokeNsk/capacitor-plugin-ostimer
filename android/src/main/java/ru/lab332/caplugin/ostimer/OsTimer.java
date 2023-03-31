d73fb10c7a5c5d337a852927ba085d1bba9c9c19
SmokeNsk <109534426+SmokeNsk@users.noreply.github.com>
2023-03-31 11:28:11 +0700

    Update OsTimer.java

--- android/src/main/java/ru/lab332/caplugin/ostimer/OsTimer.java
+++ android/src/main/java/ru/lab332/caplugin/ostimer/OsTimer.java
@@ -4,51 +4,72 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-
 public class OsTimer {
 
     OsTimerCallback timerCallback;
     long period = 1000;
-    long delay=1000;
-    private Timer mtimer =null;
+    long delay = 1000;
+    private Timer mtimer = null;
     private int mSecondPassed = 0;
 
     public OsTimer() {
     }
 
 
-    private TimerTask mTask = new TimerTask() {
+    /* private TimerTask mTask = new TimerTask() {
 
-        @Override
-        public void run() {
-            mSecondPassed++;
+         @Override
+         public void run() {
+             mSecondPassed++;
 
-            if (timerCallback != null) {
-                try {
-                    timerCallback.handle(mSecondPassed);
+             if (timerCallback != null) {
+                 try {
+                     timerCallback.handle(mSecondPassed);
 
-                } catch (Exception e) { // this shouldn't happen
-                    e.printStackTrace();
-                }
-            }
-            System.out.println("Seconds passed: " + mSecondPassed);
-        }
-    };
-
+                 } catch (Exception e) { // this shouldn't happen
+                     e.printStackTrace();
+                 }
+             }
+             System.out.println("Seconds passed: " + mSecondPassed);
+         }
+     };
+ */
     public void start(OsTimerCallback pluginCall, long delay, long period) {
         this.timerCallback = pluginCall;
 
         this.period = period;
-        this.delay=delay;
-        if (mtimer==null) {
-            mtimer = new Timer();
-            mtimer.scheduleAtFixedRate(mTask, delay, period);
-        }
+        this.delay = delay;
+        if (mtimer != null) {
+            try {
+                mtimer.cancel();
+            }catch (Exception ex){
 
+            }
+            //mtimer = null;
+        }
+        mtimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                mSecondPassed++;
+
+                if (timerCallback != null) {
+                    try {
+                        timerCallback.handle(mSecondPassed);
+
+                    } catch (Exception e) { // this shouldn't happen
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("Seconds passed: " + mSecondPassed);
+            }
+        }, delay, period);
     }
-    public void stop(){
+
+
+    public void stop() {
         try {
             mtimer.cancel();
-        }catch (Exception e){}
+        } catch (Exception e) {
+        }
     }
 }
